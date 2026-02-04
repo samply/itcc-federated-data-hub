eb13ce64e18119d9eddb38295246040c1172745d
Martin Jurk <martin.jurk@dkfz-heidelberg.de>
2026-02-04 08:17:03 +0100

    test ignored

--- itcc-omics-ingest/src/test/pseudonym.rs
+++ itcc-omics-ingest/src/test/pseudonym.rs
@@ -2,6 +2,7 @@ use crate::pseudonym::handler::create_session;
 use crate::test::{test_app_state, test_config};
 use crate::utils::error_type::ErrorType;
 
+#[ignore = "Require mainzelliste"]
 #[tokio::test]
 async fn test_pseudonym() -> Result<(), ErrorType> {
     let app_state = test_app_state();
